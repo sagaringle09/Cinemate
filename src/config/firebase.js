c27974125cb24ab0fe2e14d05b809634c890cb28
Sagar <sagar.inglee@gmail.com>
2023-08-11 17:09:35 +0530

    config fill add

---
+++ src/config/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAxtYQn6wmhRH09V8jc4BNVfocpUivDHX8",
+  authDomain: "cinemate-71e56.firebaseapp.com",
+  projectId: "cinemate-71e56",
+  storageBucket: "cinemate-71e56.appspot.com",
+  messagingSenderId: "268675023336",
+  appId: "1:268675023336:web:eeee20f9666685984c645d",
+  measurementId: "G-Y070R5WTSP",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebase;
