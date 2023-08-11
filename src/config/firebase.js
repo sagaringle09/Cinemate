a0e3621a8685257b59d6fe8bf3f1bea677744d50
Sagar <sagar.inglee@gmail.com>
2023-08-11 17:14:25 +0530

    add file

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -8,6 +8,7 @@ const firebaseConfig = {
   messagingSenderId: "268675023336",
   appId: "1:268675023336:web:eeee20f9666685984c645d",
   measurementId: "G-Y070R5WTSP",
+  REACT_APP_API_KEY: "9925ca5a34dd4a3e87afb82bbfd63b66",
 };
 
 // Initialize Firebase
